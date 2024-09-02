6e0cd971a11c8c35c374bf2c292a27c9fec5a67f
Matias Bongiovanni <matiasweschta@gmail.com>
2024-09-02 19:30:11 -0300

    feat: add Vercel Analytics and Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Home from "./pages/Home";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
+    <>
     <Home />
+    <Analytics />
+    <SpeedInsights />
+    </>
+    
   )
 }
